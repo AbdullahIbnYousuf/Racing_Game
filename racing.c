cd939661470b9631f165528e3d3d2f638394e3af
AbdullahIbnYousuf <abuabdullahibnyousuf@gmail.com>
2025-06-01 20:44:23 +0600

    scleton done

--- racing.c
+++ racing.c
@@ -1,5 +1,7 @@
 #include "stdio.h"
 #include "raylib.h"
+#include "math.h"
+#include "stdlib.h"
 #define Background_color (Color){180, 149, 127}
 #define car_color BLACK
 
@@ -10,13 +12,18 @@ int main(void)
     InitWindow(width, height, "Racer");
     SetTargetFPS(60);
 
-    int car_width = 60;
-    int car_height = 100;
+    int car_width = 30;
+    int car_height = 50;
     float car_x = width / 2 - car_width / 2;
     // float car_y = height / 2 - car_height / 2;
     float car_y = height;
     float car_speed = 0;
-    float car_max_speed = 1000;
+    float car_max_speed = 15;
+    int car_direction = 0;
+    float car_rotation = 0;
+    float rotation_speed = 20;
+    float car_speedup = 5;
+    float car_speeddown = 10;
 
     while (!WindowShouldClose())
     {
@@ -27,24 +34,80 @@ int main(void)
         DrawText("Wloiusrhei  jgr xfgnf  he  yhe kb  lr!", 190, 200, 20, LIGHTGRAY);
         if (IsKeyDown(KEY_UP))
         {
-            car_speed += 100 * dt;
+            car_direction = 1;
+            car_speed += car_speedup * dt;
             if (car_speed > car_max_speed)
+            {
+                car_speed = car_max_speed;
+            }
+        }
+        else if (IsKeyDown(KEY_DOWN))
+        {
+            car_direction = -1;
+            car_speed -= car_speedup * dt;
+            if (car_direction * car_speed > car_max_speed)
             {
                 car_speed = car_max_speed;
             }
         }
         else
         {
-            car_speed -= 30 * dt;
-            if (car_speed < 0)
+            car_speed -= car_direction * car_speeddown * dt;
+            if (car_direction * car_speed < 0)
             {
                 car_speed = 0;
             }
         }
+        if (car_speed)
+        {
+            if (IsKeyDown(KEY_LEFT))
+            {
+                car_rotation -= rotation_speed * dt * abs(car_speed);
+            }
+            else if (IsKeyDown(KEY_RIGHT))
+            {
+                car_rotation += rotation_speed * dt * abs(car_speed);
+            }
+        }
 
-        car_y -= car_speed;
+        // round up circle
+        if (car_rotation > 360)
+        {
+            car_rotation = 0;
+        }
+        else if (car_rotation < 0)
+        {
+            car_rotation = 360;
+        }
 
-        DrawRectangle(car_x, car_y, car_width, car_height, car_color);
+        float rad = PI * car_rotation / 180;
+        car_x -= car_speed * cosf(rad);
+        car_y -= car_speed * sinf(rad);
+
+        // border
+        if (car_y < 0)
+        {
+            car_y = 0;
+            // car_speed = 0;
+        }
+        else if (car_y > 900)
+        {
+            // car_speed = 0;
+            car_y = 900;
+        }
+        if (car_x < 0)
+        {
+            car_x = 0;
+            // car_speed = 0;
+        }
+        else if (car_x > 1300)
+        {
+            // car_speed = 0;
+            car_x = 1300;
+        }
+        Vector2 car_origin = {car_width / 2, car_height / 2};
+        Rectangle car_rec = {car_x, car_y, car_height, car_width};
+        DrawRectanglePro(car_rec, car_origin, car_rotation, car_color);
         EndDrawing();
     }
 
